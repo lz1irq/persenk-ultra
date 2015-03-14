a7f9890e071b59f2108af90e69f15fca7aa8c0a7
lz1irq <ivaylo.markoff@gmail.com>
2015-03-14 18:51:17 +0200

    Display runners' names and category short names in the table

--- WebContent/scripts/persenk-ultra.js
+++ WebContent/scripts/persenk-ultra.js
@@ -1,35 +1,56 @@
-$(document).ready(function() {
-	"use strict"
+var api = "http://private-43da-persenkultra.apiary-mock.com/"
 
-	var api = "http://private-43da-persenkultra.apiary-mock.com/"
-
-	// selectors
-	var tableHeaderSelector = "#table-header th:last";
-	var tableTimesSelector = "#table-times"
-
-	// populate table headers with the aid-station codes
-	var tableHeader = $(tableHeaderSelector);
+function resolve_category(category_id) {
+	var category_name = "";
 	$.ajax({
 		type : "GET",
-		url : api + "station",
-		success : function(aid_stations) {
-			$.each(aid_stations, function() {
-				tableHeader.after("<th>" + "A" + this.id + "</th>");
-				tableHeader = $(tableHeaderSelector);
-			});
+		url : api + "categories/" + category_id,
+		async: false,
+		success : function(category) {
+			category_name = category.short_name;
 		}
-	});
-	
-	//list all runners
-	var tableTimes = $(tableTimesSelector);
-	$.ajax({
-		type : "GET",
-		url : api + "runners",
-		success : function(runners) {
-			$.each(runners, function() {
-				tableTimes.append("<tr>" + "<td>" + this.id + "</td>" + "<td>" + this.name + "</td>" + "</tr>");
+	})
+	return category_name;
+}
+
+$(document).ready(
+		function() {
+			"use strict"
+
+			// selectors
+			var tableHeaderSelector = "#table-header th:last";
+			var tableTimesSelector = "#table-times"
+
+			// populate table headers with the aid-station codes
+			var tableHeader = $(tableHeaderSelector);
+			$.ajax({
+				type : "GET",
+				url : api + "station",
+				success : function(aid_stations) {
+					$.each(aid_stations, function() {
+						tableHeader.after("<th>" + "A" + this.id + "</th>");
+						tableHeader = $(tableHeaderSelector);
+					});
+				}
 			});
-		}
+
+			// list all runners
+			var tableTimes = $(tableTimesSelector);
+			$.ajax({
+				type : "GET",
+				url : api + "runners",
+				success : function(runners) {
+					$.each(runners, function() {
+						
+						var runner_data = "<tr>" + "<td>" + this.id + "</td>";
+						runner_data += "<td>" + this.name + "</td>";
+						runner_data += "<td>" + resolve_category(this.category) + "</td>";
+						
+						tableTimes.append(runner_data);
+					});
+				}
 			});
+			
+			
 
 		});
