6f865ce4a9a922782976fb0943d8cb1f3e989d1d
lz1irq <ivaylo.markoff@gmail.com>
2015-03-14 16:40:04 +0200

    Getting the list of aid stations and displaying it in the main table

--- WebContent/scripts/persenk-ultra.js
+++ WebContent/scripts/persenk-ultra.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
 	"use strict"
-	
-	function pop() {
-		
-	}
+
+	var api = "http://private-43da-persenkultra.apiary-mock.com/"
+
+	// selectors
+	var tableHeaderSelector = "#table-header th:last";
+
+	// populate table headers with the aid-station codes
+	var tableHeader = $(tableHeaderSelector);
+	$.ajax({
+		type : "GET",
+		url : api + "station",
+		success : function(aid_stations) {
+			$.each(aid_stations, function() {
+				tableHeader.after("<th>" + "A" + this.id + "</th>");
+				tableHeader = $(tableHeaderSelector);
+			});
+		}
+	});
 
 });
