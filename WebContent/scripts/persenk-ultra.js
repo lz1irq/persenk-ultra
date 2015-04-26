1bad1843d25534d848ea6158ba97470240109f0f
lz1irq <ivaylo.markoff@gmail.com>
2015-04-27 02:36:46 +0300

    Collapsible panels for different items in administrator view

--- WebContent/scripts/persenk-ultra.js
+++ WebContent/scripts/persenk-ultra.js
@@ -1,5 +1,5 @@
 "use strict"
-var apiURL = 'http://localhost:8080/persenk-ultra/api/';
+var apiURL = '/persenk-ultra/api/';
 
 var persenkUltraServices = angular.module('persenkUltraServices', [ 'ngResource' ]);
 
@@ -15,40 +15,35 @@ persenkUltraServices.factory('RunnerFactory', [ '$http', function($http) {
 	this.getRunners = function() {
 		return $http.get(apiURL + 'runners');
 	}
-	
+
 	return this;
 } ]);
 
-var persenkUltra = angular.module('persenkUltra', [ 'ngRoute', 'persenkUltraServices' ]);
-persenkUltra.config(['$routeProvider', 
-    function($routeProvider) {
-		$routeProvider.
-		when('/table', {
-			templateUrl: 'views/table.html'
-		}).
-		when('/admin', {
-			templateUrl: 'views/admin.html'
-		}).
-		when('/about', {
-			templateUrl: 'views/about.html'
-		}).
-		otherwise({
-			redirectTo: '/table'
-		});
-	}                 
-]);
+var persenkUltra = angular.module('persenkUltra', [ 'ngRoute', 'persenkUltraServices', 'ui.bootstrap' ]);
+persenkUltra.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/table', {
+		templateUrl : 'views/table.html'
+	}).when('/admin', {
+		templateUrl : 'views/admin.html'
+	}).when('/about', {
+		templateUrl : 'views/about.html'
+	}).when('/contacts', {
+		redirectTo : '/about'
+	}).otherwise({
+		redirectTo : '/table'
+	});
+} ]);
 
-persenkUltra.controller('HeaderController', ['$scope', '$location', function($scope, $location) {
-	$scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
-    };
-}]);
+persenkUltra.controller('HeaderController', [ '$scope', '$location', function($scope, $location) {
+	$scope.isActive = function(viewLocation) {
+		return viewLocation === $location.path();
+	};
+} ]);
 
 persenkUltra.controller('AidStationController', [ '$scope', 'AidStationFactory', function($scope, AidStationFactory) {
 
 	$scope.getAidStations = function() {
-		AidStationFactory.getAidStations()
-		.success(function(stations) {
+		AidStationFactory.getAidStations().success(function(stations) {
 			$scope.aidStations = stations;
 		}).error(function(errorMessage) {
 			console.log(errorMessage);
@@ -62,16 +57,21 @@ persenkUltra.controller('AidStationController', [ '$scope', 'AidStationFactory',
 
 persenkUltra.controller('RunnerController', [ '$scope', 'RunnerFactory', function($scope, RunnerFactory) {
 	$scope.getRunners = function() {
-		RunnerFactory.getRunners()
-		.success(function(runners) {
+		RunnerFactory.getRunners().success(function(runners) {
 			$scope.runners = runners;
-		})
-		.error(function(errorMessage) {
+		}).error(function(errorMessage) {
 			console.log(errorMessage);
 			$scope.status = "Unable to load aid station data: " + errorMessage;
 		});
 	}
-	
+
 	$scope.getRunners();
 
 } ]);
+
+persenkUltra.controller('adminUIController', function($scope) {
+	$scope.status = {
+		isFirstOpen : true,
+		isFirstDisabled : false
+	};
+});
