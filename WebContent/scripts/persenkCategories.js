4b68b482a4d2ad40b87d05390a2473f696402b3d
lz1irq <ivaylo.markoff@gmail.com>
2015-04-27 17:39:12 +0300

    Category factory and controller

---
+++ WebContent/scripts/persenkCategories.js
@@ -0,0 +1,63 @@
+persenkUltraServices.factory('CategoryFactory', [ '$http', function($http) {
+	
+	var categoryURL = apiURL + 'categories/';
+	
+	this.getCategories = function() {
+		return $http.get(categoryURL);
+	};
+	
+	this.deleteCategory = function(categoryId) {
+		return $http.delete(categoryURL + categoryId);
+	};
+	
+	this.createCategory = function(category) {
+		return $http.post(categoryURL, category);
+	}
+	
+	this.updateCategory = function(category) {
+		return $http.put(categoryURL + category.id, category);
+	}
+
+	return this;
+} ]);
+
+persenkUltra.controller('CategoryController', [ '$scope', 'CategoryFactory', function($scope, CategoryFactory) {
+
+	$scope.categories = [];
+			
+	$scope.getCategories = function() {
+		CategoryFactory.getCategories().success(function(stations) {
+			$scope.categories = stations;
+		}).error(function(errorMessage) {
+			console.log(errorMessage);
+			$scope.status = "Unable to load aid station data: " + errorMessage;
+		});
+	}
+	
+	$scope.deleteCategory = function(categoryId) {
+		for(var index=0;index < $scope.categories.length;index++) {
+			if($scope.categories[index].id === categoryId) {
+				$scope.categories.splice(index, 1);
+				break;
+			}
+		}
+		CategoryFactory.deleteCategory(categoryId);
+	}
+	
+	$scope.createCategory = function(category) {
+		CategoryFactory.createCategory(category).success(function(newStation) {
+			console.log(newStation);
+			$scope.categories.push(category);
+
+		}).error(function(errorMessage) {
+			console.log(errorMessage);
+		});
+	}
+	
+	$scope.updateCategory = function(category) {
+		CategoryFactory.updateCategory(category);
+	}
+
+	$scope.getCategories();
+
+} ]);
