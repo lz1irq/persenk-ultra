2e4bb679324ca73b82144623a30b31ce88303b90
lz1irq <ivaylo.markoff@gmail.com>
2015-04-27 23:01:18 +0300

    Added User entity model (to be used for security)

---
+++ src/com/persenkultra/times/model/User.java
@@ -0,0 +1,49 @@
+package com.persenkultra.times.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity(name="Users")
+@NamedQueries({@NamedQuery(name="allUsers", query="SELECT u FROM Users u")})
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable = false, length = 50)
+	private String username;
+	
+	@Column(nullable = false, length = 50)
+	private String password;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@XmlTransient
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
