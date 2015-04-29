59efe55f71834fb20b8330d39103027751d6943c
lz1irq <ivaylo.markoff@gmail.com>
2015-04-29 20:23:44 +0300

    List Runners with Category

--- WebContent/scripts/api.js
+++ WebContent/scripts/api.js
@@ -2,9 +2,11 @@
 
 var apiURL = '/persenk-ultra/api/';
 var aidStationURL = apiURL + 'stations/';
+var runnerURL = apiURL + 'runners/'
 
 var api = {}
 api.aidStations = {};
+api.Runners = {};
 
 api.aidStations.getAidStations = function(callback) {
 	var request = $.ajax({
@@ -18,4 +20,17 @@ api.aidStations.getAidStations = function(callback) {
 		}
 	});
 	
+}
+
+api.Runners.getRunners = function(callback) {
+	var request = $.ajax({
+		url : runnerURL,
+		method : 'GET',
+		dataType : "json",
+		success : function(data, status, jqXHR) {
+			if(callback !== undefined) {
+				callback(data);
+			}
+		}
+	});
 }
--- WebContent/scripts/main.js
+++ WebContent/scripts/main.js
@@ -2,14 +2,32 @@ $(document).ready(function() {
 	"use strict"
 	
 	var listAidStations = function(stations) {
-		var header = $('#runnersTable thead tr td:last');
+		var header = $('#runnerTable thead tr');
 		$.each(stations, function(index, station) {
-			var td = $('<td/>');
-			td.html('<b>' + station.name + '</b>');
-			header.after(td); 
+			var stationName = $('<td/>');
+			stationName.html('<b>' + station.name + '</b>');
+			header.append(stationName); 
 			console.log();
 		});
 	}
 	
+	var listRunners = function(runners) {
+		var holder = $('#runnerTable tbody');
+		$.each(runners, function(index, runner) {
+			
+			var row = $('<tr/>');
+			var indexData = $('<td/>').html('<b>' + (index+1) + '</b>').attr('align', 'center');
+			var name = $('<td/>').html('<b>' + runner.name + '</b>').attr('align', 'center');
+			var category = $('<td/>').html('<b>' + runner.category.shortName + '</b>').attr('align', 'center');
+			
+			row.append(indexData);
+			row.append(name);
+			row.append(category);
+			
+			holder.append(row);
+		});
+	}
+	
 	api.aidStations.getAidStations(listAidStations);
+	api.Runners.getRunners(listRunners);
 });
